005facc54f03dbd7f3f4d92cc422b0fae383f263
qiahao <404546074@qq.com>
2018-12-26 17:16:39 +0800

    reset

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -65,7 +65,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: config.build.index,
-      template: './src/index.html',
+      template: 'index.html',
       inject: true,
       // favicon: resolve('favicon.ico'),
       title: 'vue-element-admin',
